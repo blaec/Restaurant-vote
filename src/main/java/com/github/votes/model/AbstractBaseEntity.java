1d697c3ae1f6d6dfda89bf68c608756890948c16
blaec <odeskonst@yandex.ru>
2018-04-20 16:51:07 +0300

    tests FIX by returning spring-web dependency

--- src/main/java/com/github/votes/model/AbstractBaseEntity.java
+++ src/main/java/com/github/votes/model/AbstractBaseEntity.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 @MappedSuperclass
 @Access(AccessType.FIELD)
 public abstract class AbstractBaseEntity implements HasId {
-    public static final Integer START_SEQ = 100000;
+    public static final int START_SEQ = 100000;
 
     @Id
     @SequenceGenerator(name = "global_seq", sequenceName = "global_seq", allocationSize = 1, initialValue = START_SEQ)
